239b16e4e85be4201bf63c68525abcd8bd5bf7c5
Reinis Riekstins <riekstins.reinis@gmail.com>
2025-08-04 22:00:28 +0300

    code after section 51.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,14 @@
 const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
-const web3 = new Web3(ganache.provider());
+
 const { interface, bytecode } = require('../compile.js'); // Adjust the path as necessary
+
+const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+const MSG_ARG = 'Hi there!';
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -15,7 +18,7 @@ beforeEach(async () => {
 
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode, arguments: [MSG_ARG] })
     .send({ from: deployerAccount, gas: '1000000' });
 });
 
@@ -24,5 +27,15 @@ describe('Inbox', () => {
     assert.ok(inbox.options.address);
   });
 
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, MSG_ARG);
+  });
 
+  it('can change the message', async () => {
+    const newMessage = 'New message!';
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, newMessage);
+  });
 });
